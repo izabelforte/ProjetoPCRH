f7b15ea65ee48cec4a38738fb1ee79e765e6101f
izabelforte <izabelrforte@gmail.com>
2025-09-05 23:56:38 +0100

    Inicio AppDbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,37 @@
-﻿namespace ProjetoPCRH.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoPCRH.Models
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
-    }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+        public DbSet<Projeto> Projetos { get; set; }
+        public DbSet<Contrato> Contratos { get; set; }
+        public DbSet<Faturacao> Faturas { get; set; }
+        public DbSet<ProjetoFuncionario> ProjetosFuncionarios { get; set; }
+
+        public DbSet<Relatorio> Relatorios { get; set; }
+        public DbSet<Utilizador> Utilizadores { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Add your model configurations here
+           
+            
+
+        }
+    
+}
 }
