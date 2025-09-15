728e7ccfccbc3e124a8a1b4d3953c6de18dd9163
joaomdpaz <joaomdpaz@gmail.com>
2025-09-15 20:01:07 +0100

    Criação do terminar e alterações associadas

--- Models/Relatorio.cs
+++ Models/Relatorio.cs
@@ -10,16 +10,13 @@ namespace ProjetoPCRH.Models
         [Key]
         public int RelatorioId { get; set; }
 
-        [DisplayName("Data")]
         [Column(TypeName = "date")]  // para guardar so a data sem horas
         public DateTime DataRelatorio { get; set; }
 
-        [DisplayName("Valor")]
         public double Valor { get; set; }
         
         public int TempoTotalHoras { get; set; }
 
-        [ForeignKey("ProjetoId")]
         public int ProjetoId { get; set; }
         public Projeto Projeto { get; set; }
     }
